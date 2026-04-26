3b67c6f8dfe70c4a35c2142379fc2b55431949eb
GoldenGlint <darhgo@gmail.com>
2026-04-25 23:48:22 -0400

    Fix Image Loading

--- src/home-page.js
+++ src/home-page.js
@@ -1,3 +1,14 @@
+import bubbleTeaImg from "./photos/bubble-tea.jpg";
+import congeeImg from "./photos/congee.jpg";
+import eggTartsImg from "./photos/egg-tarts.jpg";
+import friedRiceImg from "./photos/fried-rice.jpg";
+import harGowImg from "./photos/har-gow.jpg";
+import noodlesImg from "./photos/noodles.jpg";
+import porkBellyImg from "./photos/pork-belly.jpg"
+import stirFryVeggiesImg from "./photos/stir-fry-veggies.jpg"
+import xiaoLongBaoImg from "./photos/xiao-long-bao.jpg";
+
+
 export const homepage = () => {
 //create middle
 const middle=document.createElement("div");
@@ -17,41 +28,42 @@ middle.appendChild(learnButton);
 //create image grid
 
 const photoGrid=document.createElement("div");
+photoGrid.id="photo-grid";
 
 const bubbleTea=document.createElement("img");
-bubbleTea.src="./photos/bubble-tea.jpg"
+bubbleTea.src=bubbleTeaImg;
 bubbleTea.alt = 'bubble tea';
 
 const congee=document.createElement("img");
-congee.src="./photos/congee.jpg"
+congee.src=congeeImg;
 congee.alt = 'congee';
 
 const eggTarts=document.createElement("img");
-eggTarts.src="./photos/egg-tarts.jpg"
+eggTarts.src=eggTartsImg;
 eggTarts.alt = 'egg tarts';
 
 const friedRice=document.createElement("img");
-friedRice.src="./photos/fried-rice.jpg"
+friedRice.src=friedRiceImg;
 friedRice.alt = 'fried rice';
 
 const harGow=document.createElement("img");
-harGow.src="./photos/har-gow.jpg"
+harGow.src=harGowImg;
 harGow.alt = 'har gow';
 
 const noodles=document.createElement("img");
-noodles.src="./photos/noodles.jpg"
+noodles.src=noodlesImg;
 noodles.alt = 'noodles';
 
 const porkBelly=document.createElement("img");
-porkBelly.src="./photos/pork-belly.jpg"
+porkBelly.src=porkBellyImg;
 porkBelly.alt = 'pork belly';
 
 const stirFryVeggies=document.createElement("img");
-stirFryVeggies.src="./photos/stir-fry-veggies.jpg"
+stirFryVeggies.src=stirFryVeggiesImg;
 stirFryVeggies.alt = 'stir fry veggies';
 
 const xiaoLongBao=document.createElement("img");
-xiaoLongBao.src="./photos/xiao-long-bao.jpg"
+xiaoLongBao.src=xiaoLongBaoImg;
 xiaoLongBao.alt = 'xiao long bao';
 
 photoGrid.appendChild(bubbleTea);
